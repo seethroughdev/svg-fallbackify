1ba4d1ab5e8697b7278c1be7a5f6158c17223c33
Adam L <github@adamin.net>
2014-09-30 00:23:22 -0700

    Added no-svg fix

--- src/jquery.svg-fallbackify.js
+++ src/jquery.svg-fallbackify.js
@@ -13,16 +13,75 @@
     this.init(defaults);
   }
 
-  // we want to make sure the filename we're replacing is an SVG
-  function isSvg(filename) {
+  // check that the filename we're replacing is an SVG
+  function isSvg(val) {
     var reg = /\.svg$/ig;
-    return reg.test(filename);
+    return reg.test(val);
+  }
+
+  // check if data-svg-fallback HAS image extension
+  function hasExt(val) {
+    var reg = /\.(?:jpg|gif|png|svg|jpeg)$/i;
+    return val.match(reg);
+  }
+
+  // check if data-svg-fallback IS image extension
+  function isExt(val) {
+  	var reg = /^(?:jpg|gif|png|svg|jpeg)$/i;
+  	return val.match(reg);
+  }
+
+  // replace svg extension with another
+  function replaceSvg(val, ext) {
+    var reg = /(svg)$/i;
+    return val.replace(reg, ext);
+  }
+
+  // replace img src with expected value
+  function getFallback($el, val, ext) {
+    if (val === '') {
+      $el.attr('src', replaceSvg($el.attr('src'), ext));
+    } else if (isExt(val)) {
+      $el.attr('src', replaceSvg($el.attr('src'), val));
+    } else if (isSvg(val)) {
+      $el.attr('src', replaceSvg(val, ext));
+    } else if (!hasExt(val)) {
+      $el.attr('src', val + '.' + ext);
+    } else {
+      $el.attr('src', val);
+    }
   }
 
   $.extend(Plugin.prototype, {
+
     init: function(defaults) {
-    	console.log(defaults)
+    	var $el = $(this.element)
+      ,   fallbackVal = $el.data('svg-fallback')
+    	,   srcVal = $el.attr('src')
+      ,   supportsSvg = this.supportsSvg();
+
+    	if (typeof fallbackVal === 'undefined') {
+        console.log('none');
+    		return;
+    	}
+
+      // if no src and supports svg and fallback has no ext
+      if (supportsSvg && !isSvg(srcVal) && !hasExt(fallbackVal)) {
+        getFallback($el, fallbackVal, 'svg');
+      }
+
+      // if svg src is not svg, get fallback
+      if (supportsSvg && !isSvg(srcVal) && isSvg(fallbackVal)) {
+        getFallback($el, fallbackVal, 'svg');
+      }
+
+      // if does not support svg, replace with fallback
+      if (!supportsSvg) {
+        getFallback($el, fallbackVal, this.settings.defaultExt);
+      }
+
     },
+
     supportsSvg: function() {
       return !!document && !!document.createElementNS &&
         !!document.createElementNS("http://www.w3.org/2000/svg", "svg").createSVGRect;
