53c47ac23878f59f7e0e0a842801cb2ef3de7680
Adam L <github@adamin.net>
2014-09-29 21:02:41 -0700

    Initial commit

---
+++ dist/jquery.boilerplate.js
@@ -0,0 +1,51 @@
+/*
+ *  jQuery Boilerplate - v3.3.4
+ *  A jump-start for jQuery plugins development.
+ *  http://jqueryboilerplate.com
+ *
+ *  Made by Zeno Rocha
+ *  Under MIT License
+ */
+/*=============================================
+=  Started from the awesome jQuery boilerplate  =
+= http://jqueryboilerplate.com/ =
+=============================================*/
+
+
+;(function ( $, window, document, undefined ) {
+
+		var pluginName = "svgFallback",
+				defaults = {
+					propertyName: "value"
+				};
+
+		function Plugin ( element, options ) {
+				this.element = element;
+				this.settings = $.extend( {}, defaults, options );
+				this._defaults = defaults;
+				this._name = pluginName;
+				this.init();
+		}
+
+		// Avoid Plugin.prototype conflicts
+		$.extend(Plugin.prototype, {
+				init: function () {
+
+				},
+				yourOtherFunction: function () {
+						// some logic
+				}
+		});
+
+		$.fn[ pluginName ] = function ( options ) {
+				this.each(function() {
+						if ( !$.data( this, "plugin_" + pluginName ) ) {
+								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+						}
+				});
+
+				// chain jQuery functions
+				return this;
+		};
+
+})( jQuery, window, document );
